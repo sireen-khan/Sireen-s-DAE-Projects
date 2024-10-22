f020f6213e44d0f8f3b8a7f9a27c5ea5398d8cd2
sireen-khan <sireenk09@gmail.com>
2024-10-22 16:28:41 -0400

    This is a function

---
+++ python_1/functions/functions.py
@@ -0,0 +1,13 @@
+def bark():
+    print( "A dog barked Woof woof woof!" )
+    print( "Big wooof!" )
+    
+def meow():
+    print( "A cat meowed meow meow meow" )
+    
+meow()
+bark()
+meow()
+bark()
+bark()
+meow()
